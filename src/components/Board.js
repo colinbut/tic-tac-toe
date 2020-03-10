e2b419b92788bc841e2d32c7cd6c7e4d6f6f0461
Colin But <colin.but@outlook.com>
2020-03-10 08:39:35 +0000

    Passing props to display number box

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,8 +3,8 @@ import Square from './Square'
 
 class Board extends Component {
 
-    renderSquare() {
-        return <Square/>;
+    renderSquare(i) {
+        return <Square value={i} />;
     }
 
     render() {
@@ -15,19 +15,19 @@ class Board extends Component {
             <div>
                 <div className="status">{status}</div>
                 <div className="board-row">
-                    {this.renderSquare()}
-                    {this.renderSquare()}
-                    {this.renderSquare()}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                    {this.renderSquare(3)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare()}
-                    {this.renderSquare()}
-                    {this.renderSquare()}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                    {this.renderSquare(6)}
                 </div>
                 <div className="board-row">
-                    {this.renderSquare()}
-                    {this.renderSquare()}
-                    {this.renderSquare()}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                    {this.renderSquare(9)}
                 </div>
             </div>
         )
