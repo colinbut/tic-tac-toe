69de54721abb1f8e582fc27435bf847f70c4f255
Colin But <colin.but@outlook.com>
2020-03-18 21:50:08 +0000

    Lifting state up from Square to Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,8 +3,26 @@ import Square from './Square'
 
 class Board extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X'
+        this.setState({squares: squares})
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square 
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
@@ -15,19 +33,19 @@ class Board extends Component {
             <div>
                 <div className="status">{status}</div>
                 <div className="board-row">
+                    {this.renderSquare(0)}
                     {this.renderSquare(1)}
                     {this.renderSquare(2)}
-                    {this.renderSquare(3)}
                 </div>
                 <div className="board-row">
+                    {this.renderSquare(3)}
                     {this.renderSquare(4)}
                     {this.renderSquare(5)}
-                    {this.renderSquare(6)}
                 </div>
                 <div className="board-row">
+                    {this.renderSquare(6)}
                     {this.renderSquare(7)}
                     {this.renderSquare(8)}
-                    {this.renderSquare(9)}
                 </div>
             </div>
         )
